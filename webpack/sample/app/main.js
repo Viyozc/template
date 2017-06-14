ed030b9cc1433fd35daa08b124ae44ab4383aad0
allenzc <chisny@163.com>
2017-06-14 15:46:02 +0800

    postcss

--- webpack/sample/app/main.js
+++ webpack/sample/app/main.js
@@ -1,3 +1,5 @@
 //main.js
+require('style.css');
+require('stylus.styl');
 var greeter = require('./Greeter.js');
 document.getElementById('root').appendChild(greeter());
--- webpack/sample/build/main.js
+++ webpack/sample/build/main.js
@@ -70,20 +70,13 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-// Greeter.js
-module.exports = function() {
-    var greet = document.createElement('div');
-    greet.textContent = "Hi there and greetings!";
-    return greet;
-};
+eval("// Greeter.js\nmodule.exports = function () {\n    var greet = document.createElement('div');\n    greet.textContent = \"Hi there and greetings!\";\n    return greet;\n};//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hcHAvR3JlZXRlci5qcz9lYjg0Il0sIm5hbWVzIjpbIm1vZHVsZSIsImV4cG9ydHMiLCJncmVldCIsImRvY3VtZW50IiwiY3JlYXRlRWxlbWVudCIsInRleHRDb250ZW50Il0sIm1hcHBpbmdzIjoiQUFBQTtBQUNBQSxPQUFPQyxPQUFQLEdBQWlCLFlBQVc7QUFDeEIsUUFBSUMsUUFBUUMsU0FBU0MsYUFBVCxDQUF1QixLQUF2QixDQUFaO0FBQ0FGLFVBQU1HLFdBQU4sR0FBb0IseUJBQXBCO0FBQ0EsV0FBT0gsS0FBUDtBQUNILENBSkQiLCJmaWxlIjoiMC5qcyIsInNvdXJjZXNDb250ZW50IjpbIi8vIEdyZWV0ZXIuanNcbm1vZHVsZS5leHBvcnRzID0gZnVuY3Rpb24oKSB7XG4gICAgdmFyIGdyZWV0ID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnZGl2Jyk7XG4gICAgZ3JlZXQudGV4dENvbnRlbnQgPSBcIkhpIHRoZXJlIGFuZCBncmVldGluZ3MhXCI7XG4gICAgcmV0dXJuIGdyZWV0O1xufTtcblxuXG4vLyBXRUJQQUNLIEZPT1RFUiAvL1xuLy8gLi9hcHAvR3JlZXRlci5qcyJdLCJzb3VyY2VSb290IjoiIn0=");
 
 /***/ }),
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-//main.js
-var greeter = __webpack_require__(0);
-document.getElementById('root').appendChild(greeter());
+eval("//main.js\n__webpack_require__(!(function webpackMissingModule() { var e = new Error(\"Cannot find module \\\"style.css\\\"\"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));\n// require('stylus.styl');\nvar greeter = __webpack_require__(0);\ndocument.getElementById('root').appendChild(greeter());//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hcHAvbWFpbi5qcz82YTRiIl0sIm5hbWVzIjpbInJlcXVpcmUiLCJncmVldGVyIiwiZG9jdW1lbnQiLCJnZXRFbGVtZW50QnlJZCIsImFwcGVuZENoaWxkIl0sIm1hcHBpbmdzIjoiQUFBQTtBQUNBLG1CQUFBQSxDQUFRLHFJQUFSO0FBQ0E7QUFDQSxJQUFJQyxVQUFVLG1CQUFBRCxDQUFRLENBQVIsQ0FBZDtBQUNBRSxTQUFTQyxjQUFULENBQXdCLE1BQXhCLEVBQWdDQyxXQUFoQyxDQUE0Q0gsU0FBNUMiLCJmaWxlIjoiMS5qcyIsInNvdXJjZXNDb250ZW50IjpbIi8vbWFpbi5qc1xucmVxdWlyZSgnc3R5bGUuY3NzJyk7XG4vLyByZXF1aXJlKCdzdHlsdXMuc3R5bCcpO1xudmFyIGdyZWV0ZXIgPSByZXF1aXJlKCcuL0dyZWV0ZXIuanMnKTtcbmRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdyb290JykuYXBwZW5kQ2hpbGQoZ3JlZXRlcigpKTtcblxuXG4vLyBXRUJQQUNLIEZPT1RFUiAvL1xuLy8gLi9hcHAvbWFpbi5qcyJdLCJzb3VyY2VSb290IjoiIn0=");
 
 /***/ })
 /******/ ]);
